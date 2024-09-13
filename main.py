2dfd31827869247ac521fcc79c18a602130cb6b6
SupKittyMeow <77246128+SupKittyMeow@users.noreply.github.com>
2024-09-12 18:18:03 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+import os
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.all()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='/', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Ready and logged in on {bot.user}')
+
+@bot.event
+async def on_member_update(before, after):
+        for role in after.roles:
+            if role.name == 'hates hampter':
+                try:
+                    await after.kick(reason='You wanted this!')
+                    print('Kicked!')
+                except discord.errors.Forbidden:
+                     print('Oops... they are too strong to be kicked!')
+                break
+
+
+bot.run(os.environ['BOT_TOKEN'])
